59fe9c76652eb46597b696f3d85cf7ea6f6e3e68
TheRealOne78 <bajcsielias78@gmail.com>
2022-12-31 01:12:08 +0200

    basic prototype UI loader and UI changes

---
+++ src/info.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 TheRealOne78 <bajcsielias78@gmail.com>
+ *
+ * This file is part of the XAWP project
+ *
+ * XAWP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XAWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XAWP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AUTHOR
+  #define AUTHOR "TheRealOne78"
+#endif
+
+#ifndef AUTHORMAIL
+  #define AUTHORMAIL "bajcsielias78@gmail.com"
+#endif
+
+#ifndef VERSION
+  #define VERSION "0.0.2"
+#endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,54 @@
+/*
+ * Copyright (C) 2022 TheRealOne78 <bajcsielias78@gmail.com>
+ *
+ * This file is part of the XAWP project
+ *
+ * XAWP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XAWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XAWP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Gtk */
+#include <gtk/gtk.h>
+
+/* Config */
+#include <libconfig.h>
+
+/* Basic programming */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <gtk/gtk.h>
-#include <libconfig.h>
+int main(int argc, char **argv) {
+  GObject *main_window;
+  GError *error = NULL;
+  GtkBuilder *builder_main;
+  GtkBuilder *builder_about_info;
 
-int main() {
+  gtk_init(&argc, &argv);
 
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder_main = gtk_builder_new();
+  builder_about_info = gtk_builder_new();
+  if(gtk_builder_add_from_file(builder_main, "./ui-resources/main.ui", &error) == 0 &&
+     gtk_builder_add_from_file(builder_about_info, "./ui-resources/about-info.ui", &error) == 0) {
+    g_printerr("Error loading file: %s\n", error->message);
+    g_clear_error(&error);
+    exit(EXIT_FAILURE);
+  }
+
+  /* Connect signal handlers to the constructed widgets. */
+  main_window = gtk_builder_get_object(builder_main, "window");
+  g_signal_connect(main_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_main();
 }
