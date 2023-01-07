2659a05886817b434d3451fd7894bdeffc124b32
TheRealOne78 <bajcsielias78@gmail.com>
2023-01-07 15:13:21 +0200

    using deprecated G_APPLICATION_FLAGS_NONE because of compability issue

--- src/XAWP-gui.c
+++ src/XAWP-gui.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("net.gui.XAWP", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new("net.gui.XAWP", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
